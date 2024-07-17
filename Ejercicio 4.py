65206957b328a107aa5021782e5bb83751891262
Karen <karen1.porras@gmail.com>
2024-07-17 15:50:39 -0600

    Agregar ejercicio_4

---
+++ Ejercicio 4.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 17:35:05 2024
+
+@author: Karen
+"""
+
+import requests
+import pandas as pd 
+import json
+
+#Endpoint del API 
+url = "https://dogapi.dog/api/v2/breeds"
+
+#solicitud GET
+respuesta = requests.get(url) 
+
+#verificar solicitud exitosa 
+if respuesta.status_code == 200:
+    print('Solicitud exitosa')
+    print('Datos:', respuesta.json())
+else:
+    print("Error en la solicitud del recurso. Detalles:\n",
+          respuesta.text)
+    
+#respuesta_json = respuesta_json()
+
+print(respuesta.json())
+
+
+
